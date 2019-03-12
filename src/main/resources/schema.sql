7c64bb367c6b1633ba81576e53d4d40770067b4f
RyanRush <ryan.rush4@gmail.com>
2019-03-12 13:10:55 +0000

    amend embedded database schema config to auto-generate id

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS cake (
-  id int NOT NULL,
+  id int NOT NULL AUTO_INCREMENT,
   title varchar(5000) NOT NULL,
   description varchar(5000) NOT NULL,
   image varchar(5000) NOT NULL
